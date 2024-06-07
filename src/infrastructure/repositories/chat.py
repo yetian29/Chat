8dfc054aa0d286cdf59f19aa9c179c38e2442f56
yetian29 <dathien000@gmail.com>
2024-06-07 09:17:49 +0700

    Initial

--- src/infrastructure/repositories/chat.py
+++ src/infrastructure/repositories/chat.py
@@ -15,7 +15,11 @@ class MemoryChatRepository(BaseChatRespository):
     _saved_chats: Set[Chat] = field(default_factory=set, kw_only=True)
 
     def check_chat_exist_by_title(self, title: str) -> bool:
-        for chat in self._saved_chats:
-            if chat.title.as_string() == title:
-                return True
-        return False
+        try:
+            return bool(
+                next(
+                    chat for chat in self._saved_chats if chat.title.as_string == title
+                )
+            )
+        except StopIteration:
+            return False
