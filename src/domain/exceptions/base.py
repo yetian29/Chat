5f261b8f36ee6d31051dc526a263bec67f7a46e0
yetian29 <dathien000@gmail.com>
2024-06-17 11:24:32 +0700

    Write Exeption

--- src/domain/exceptions/base.py
+++ src/domain/exceptions/base.py
@@ -1,3 +1,4 @@
 class BaseAppException(Exception):
     def __init__(self, message: str = "Application occured exception"):
+        self.message = message
         super().__init__(message)
--- src/logic/exceptions/base.py
+++ src/logic/exceptions/base.py
@@ -4,4 +4,5 @@ from src.domain.exceptions.base import BaseAppException
 class LogicException(BaseAppException):
 
     def __init__(self, message: str = "Error occured while processing request"):
+        self.message = message
         super().__init__(message)
