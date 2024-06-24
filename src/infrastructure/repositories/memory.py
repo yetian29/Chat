e57cc6fc139c439d60a1a62aa9e9b9f6fb527802
yetian29 <dathien000@gmail.com>
2024-06-24 17:09:15 +0700

    Add

--- src/infrastructure/repositories/memory.py
+++ src/infrastructure/repositories/memory.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import dataclass, field
 from typing import Optional
 
@@ -9,13 +10,16 @@ from src.infrastructure.repositories.filters.chat import GetChatsFilters
 @dataclass
 class MemoryChatRepository(BaseChatRepository):
     _chats: dict[str, Chat] = field(default_factory=dict, kw_only=True)
-    _title_index: dict[str, list[Chat]] = field(default_factory=dict, kw_only=True)
+    # _title_index: dict[str, list[Chat]] = field(default_factory=dict, kw_only=True)
+    _title_index: dict[str, list[Chat]] = field(
+        default_factory=lambda: defaultdict(list), kw_only=True
+    )
 
     def save_chat(self, chat: Chat) -> None:
         self._chats[chat.oid] = chat
         title = chat.title.as_generic_type()
-        if title not in self._title_index:
-            self._title_index[title] = []
+        # if title not in self._title_index:
+        #     self._title_index[title] = []
         self._title_index[title].append(chat)
 
     def get_chat_by_title(self, title: str) -> Optional[list[Chat]]:
--- src/logic/mediator/command.py
+++ src/logic/mediator/command.py
@@ -1,4 +1,11 @@
+from collections import defaultdict
+from dataclasses import dataclass, field
+
+from src.logic.commands.base import CT, BaseCommandHandler
 
 
+@dataclass
 class CommandMediator:
-...
+    commands_map: dict[CT, list[BaseCommandHandler]] = field(
+        default_factory=lambda: defaultdict(list)
+    )
